622b45b0f5deb69bfaf1714b195df83a3deb6c4b
Julia Zagumennaya <ulya.200199@gmail.com>
2023-03-29 19:16:14 +0300

    fourthtask

--- Program.cs
+++ Program.cs
@@ -77,3 +77,20 @@ else
     Console.WriteLine(number + "; -> НЕТ!");
 }*/
 
+/*Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
+
+Console.Write("Введите число: ");
+
+int number = int.Parse(Console.ReadLine());
+int evenNumber = 2;
+
+if(number > 1)
+{
+    while(evenNumber <= number)
+    {
+        Console.Write(evenNumber + " ");
+        evenNumber = evenNumber + 2;
+    }
+}
